f2eea3eae587a68bff03db5788a3dae7631c31d5
Farkhadov95 <farkhadov950405@gmail.com>
2023-09-21 14:30:08 +0100

    Cleaning up

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,7 +1,7 @@
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
-import SimilarProducts from '../components/product/similar-products';
-import Reviews from '../components/product/product-reviews';
+import SimilarProducts from '../components/similar-products/similar-products';
+import Reviews from '../components/product-reviews/product-reviews';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import {
