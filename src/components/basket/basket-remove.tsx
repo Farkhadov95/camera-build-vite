946de5cc5f415951935ed76b570485ec8a729ec7
Farkhadov95 <farkhadov950405@gmail.com>
2023-11-05 14:21:41 +0000

    Added FocusTrap and NoScroll

--- src/components/basket/basket-remove.tsx
+++ src/components/basket/basket-remove.tsx
@@ -6,6 +6,7 @@ import {
   removeBasketItem,
 } from '../../store/catalog-data/catalog-data';
 import { Link } from 'react-router-dom';
+import FocusTrap from 'focus-trap-react';
 
 const BasketRemoveModal = () => {
   const dispatch = useAppDispatch();
@@ -27,6 +28,17 @@ const BasketRemoveModal = () => {
     };
   }, [dispatch, handleClose]);
 
+  useEffect(() => {
+    if (product !== null) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, [product]);
+
   if (product === null) {
     return null;
   }
@@ -43,67 +55,77 @@ const BasketRemoveModal = () => {
   } = product;
 
   return (
-    <div className="modal is-active">
-      <div className="modal__wrapper">
-        <div className="modal__overlay"></div>
-        <div className="modal__content">
-          <p className="title title--h4">Удалить этот товар?</p>
-          <div className="basket-item basket-item--short">
-            <div className="basket-item__img">
-              <picture>
-                <source
-                  type="image/webp"
-                  srcSet={`/${previewImgWebp}, /${previewImgWebp2x}, 2x`}
-                />
-                <img
-                  src={`/${previewImg}`}
-                  srcSet={`/${previewImg2x} 2x`}
-                  width="140"
-                  height="120"
-                  alt={name}
-                />
-              </picture>
+    <FocusTrap
+      focusTrapOptions={{
+        initialFocus: '#deleteBtn',
+        tabbableOptions: {
+          displayCheck: 'none',
+        },
+      }}
+    >
+      <div className="modal is-active">
+        <div className="modal__wrapper">
+          <div className="modal__overlay" onClick={handleClose}></div>
+          <div className="modal__content" onClick={(e) => e.stopPropagation()}>
+            <p className="title title--h4">Удалить этот товар?</p>
+            <div className="basket-item basket-item--short">
+              <div className="basket-item__img">
+                <picture>
+                  <source
+                    type="image/webp"
+                    srcSet={`/${previewImgWebp}, /${previewImgWebp2x}, 2x`}
+                  />
+                  <img
+                    src={`/${previewImg}`}
+                    srcSet={`/${previewImg2x} 2x`}
+                    width="140"
+                    height="120"
+                    alt={name}
+                  />
+                </picture>
+              </div>
+              <div className="basket-item__description">
+                <p className="basket-item__title">{name}</p>
+                <ul className="basket-item__list">
+                  <li className="basket-item__list-item">
+                    <span className="basket-item__article">Артикул:</span>{' '}
+                    <span className="basket-item__number">{vendorCode}</span>
+                  </li>
+                  <li className="basket-item__list-item">{type} фотокамера</li>
+                  <li className="basket-item__list-item">{level} уровень</li>
+                </ul>
+              </div>
             </div>
-            <div className="basket-item__description">
-              <p className="basket-item__title">{name}</p>
-              <ul className="basket-item__list">
-                <li className="basket-item__list-item">
-                  <span className="basket-item__article">Артикул:</span>{' '}
-                  <span className="basket-item__number">{vendorCode}</span>
-                </li>
-                <li className="basket-item__list-item">{type} фотокамера</li>
-                <li className="basket-item__list-item">{level} уровень</li>
-              </ul>
+            <div className="modal__buttons">
+              <button
+                id="deleteBtn"
+                className="btn btn--purple modal__btn modal__btn--half-width"
+                type="button"
+                onClick={() => dispatch(removeBasketItem(product.id))}
+              >
+                Удалить
+              </button>
+              <Link
+                className="btn btn--transparent modal__btn modal__btn--half-width"
+                to="/"
+              >
+                Продолжить покупки
+              </Link>
             </div>
-          </div>
-          <div className="modal__buttons">
             <button
-              className="btn btn--purple modal__btn modal__btn--half-width"
+              className="cross-btn"
               type="button"
-              onClick={() => dispatch(removeBasketItem(product.id))}
+              aria-label="Закрыть попап"
+              onClick={() => handleClose()}
             >
-              Удалить
+              <svg width="10" height="10" aria-hidden="true">
+                <use xlinkHref="#icon-close"></use>
+              </svg>
             </button>
-            <Link
-              className="btn btn--transparent modal__btn modal__btn--half-width"
-              to="/"
-            >
-              Продолжить покупки
-            </Link>
           </div>
-          <button
-            className="cross-btn"
-            type="button"
-            aria-label="Закрыть попап"
-            onClick={() => handleClose()}
-          >
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
         </div>
       </div>
-    </div>
+    </FocusTrap>
   );
 };
 
